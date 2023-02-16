fe038fca48a0e2b59a66c5fe6be63fbb0ed29e84
Valeria <Valcon4717@gmail.com>
2023-02-16 16:44:22 -0700

    set up skeleton, create navbar and deleted unnecessary files

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+        <head>
+            <title>DEV | Bandit Game Studio</title>
+        </head>
+
+    <body>
+        <div id = "hi"></div>
+        <h1> hi</h1>
+        <script src="App.js"></script>
+    </body>
+    </div>
+  )
+}
